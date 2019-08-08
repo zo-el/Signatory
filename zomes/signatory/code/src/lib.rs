06a2576d8769304108908529ba9b654d5cfab6f0
zo-el <joelulahanna@gmail.com>
2019-08-08 01:03:22 -0500

    new ui

--- zomes/signatory/code/src/lib.rs
+++ zomes/signatory/code/src/lib.rs
@@ -18,20 +18,25 @@ use hdk::holochain_json_api::{
     json::JsonString,
 };
 
-use hdk::
-    holochain_wasm_utils::api_serialization::{
-        keystore::KeyType,
+use hdk::holochain_wasm_utils::api_serialization::{
+        keystore::{
+            KeyType,
+            KeystoreListResult
+        },
 };
 
 pub fn handle_sign_message(sign_by_id:String,message:String) -> ZomeApiResult<Signature> {
     hdk::keystore_sign(sign_by_id.to_string(), message.to_string()).map(Signature::from)
 }
 
-pub fn handle_create_key(id:String) -> ZomeApiResult<String> {
-// Create Revocation key
-    let rev_key = hdk::keystore_derive_key("rev_seed".to_string(), id.to_string(), KeyType::Signing)?;
+pub fn handle_create_key(seed_id:String, key_id:String) -> ZomeApiResult<String> {
+    let rev_key = hdk::keystore_derive_key(seed_id.to_string(), key_id.to_string(), KeyType::Signing)?;
     hdk::debug(format!("Revocation Key 1 : {:}",rev_key).to_string())?;
     Ok(rev_key)
+}
+
+pub fn handle_get_all_keys() -> ZomeApiResult<KeystoreListResult> {
+    hdk::keystore_list()
 }
 
 define_zome! {
@@ -46,13 +51,18 @@ define_zome! {
             handler: handle_sign_message
         }
         derive_key: {
-            inputs: | id:String |,
+            inputs: | seed_id:String, key_id:String |,
             outputs: |result: ZomeApiResult<String>|,
             handler: handle_create_key
+        }
+        get_all_keys: {
+            inputs: | |,
+            outputs: |result: ZomeApiResult<KeystoreListResult>|,
+            handler: handle_get_all_keys
         }
     ]
 
     traits: {
-        hc_public [sign]
+        hc_public [sign,derive_key,get_all_keys]
     }
 }
